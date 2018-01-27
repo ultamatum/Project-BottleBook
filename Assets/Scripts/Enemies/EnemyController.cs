a53d971260d49367ce56fd977badfc9670a7e4d0
Ultamatum <alexanderg0502@gmail.com>
2018-01-27 04:12:47 +0000

    Enemy AI basics
    Finished enemy AI works to an extent XD

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EnemyMotor))]
 public class EnemyController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Transform[] towers = new Transform[5];
+
+	EnemyMotor motor;
+
+	void Start () 
+	{
+		motor = GetComponent<EnemyMotor> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update () 
+	{
+		float shortestDist = 10000;
+		Vector3 closestTower = Vector3.zero;
 		
+		for(int i = 0; i < towers.Length; i++)
+		{
+			float distance = Vector3.Distance (transform.position, towers [i].position);
+			if(distance < shortestDist)
+			{
+				shortestDist = distance;
+				closestTower = towers [i].position;
+			}
+		}
+
+		motor.MoveToPoint (closestTower);
 	}
 }
