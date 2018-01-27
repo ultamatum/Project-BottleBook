5bf3933fa34e55cafd74b9768805dba4a53ad748
Ultamatum <alexanderg0502@gmail.com>
2018-01-27 21:57:50 +0000

    TRYING TO FIX

--- Assets/Scripts/Character/GunController.cs
+++ Assets/Scripts/Character/GunController.cs
@@ -3,7 +3,15 @@
 public class GunController : MonoBehaviour {
 
 	public float damage = 10;
+	public float fireRate = 15f;
+	public float impactForce = 30f;
 	public float range = 100f;
+
+	public ParticleSystem muzzleFlash;
+	public GameObject impactEffect;
+	public LayerMask rayMask;
+
+	private float nextFireTime = 0f;
 
 	Camera camera;
 
@@ -14,14 +22,34 @@ public class GunController : MonoBehaviour {
 
 	void Update () 
 	{
-		if (Input.GetButtonDown ("Fire1"))
+		if (Input.GetButton ("Fire1") && Time.time >= nextFireTime)
 		{
+			nextFireTime = Time.time + 1f / fireRate;
 			Shoot ();
 		}
 	}
 
 	void Shoot ()
 	{
-		
+		muzzleFlash.Play ();
+
+		RaycastHit hit;
+		if(Physics.Raycast (camera.transform.position, camera.transform.forward, out hit, range, rayMask))
+		{
+			EnemyController enemy = hit.transform.GetComponent<EnemyController> ();
+			if(enemy != null)
+			{
+				enemy.Damage (damage);
+			}
+
+			if(hit.rigidbody != null)
+			{
+				hit.rigidbody.AddForce (-hit.normal * impactForce);
+			}
+
+			GameObject impactGO = Instantiate (impactEffect, hit.point, Quaternion.LookRotation (hit.normal));
+			impactGO.transform.Rotate (90, 0, 0);
+			Destroy (impactGO, 2f);
+		}
 	}
 }
